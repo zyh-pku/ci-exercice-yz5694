c757361424e4e7c4b9ffe5f571c2847ad1e2803e
zyh-pku <yz5694@princeton.edu>
2024-11-07 20:56:49 +0000

    fix noxfile and pass ruff format test

--- noxfile.py
+++ noxfile.py
@@ -1,11 +1,14 @@
 import nox
 
+
 @nox.session
 def tests(session):
     session.install(".[test]")
     session.run("pytest")
 
+
 @nox.session
-def docs(session):
+def docs(session: nox.Session) -> None:
     session.install(".[docs]")
-    session.run("sphinx-build", "docs", "docs/_build")
+    session.chdir("docs")
+    session.run("sphinx-build", "-M", "html", ".", "build")
